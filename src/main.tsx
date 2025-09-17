08852e8a534eca4f2766055c75a79fa156d49276
Rosher Mur <rogermur1990@gmail.com>
2025-09-17 12:36:04 -0500

    Corregir helmets de react en el main

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
-// src/main.tsx
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
+import { HelmetProvider } from "react-helmet-async";
 
-// Fuentes
-import "@fontsource/space-grotesk/700.css";
-import "@fontsource/poppins/600.css";
-import "@fontsource/inter/400.css";
-
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
+  </React.StrictMode>
+);
