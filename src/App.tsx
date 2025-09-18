156592517c7a4da79d5a91be6e388a2843691b67
Rosher Mur <rogermur1990@gmail.com>
2025-09-18 14:52:45 -0500

    volver a lo que sirve

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "@/components/Footer";
 import CookieNotice from "@/components/CookieNotice";
 import ScrollToTop from "@/components/ScrollToTop";
@@ -39,55 +39,30 @@ const App = () => (
         <div className="flex flex-col min-h-screen">
           <div className="flex-1">
             <Routes>
-              {/* Trailing slash redirects - FIRST */}
-              <Route path="/about/" element={<Navigate to="/about" replace />} />
-              <Route path="/services/" element={<Navigate to="/services" replace />} />
-              <Route path="/case-studies/" element={<Navigate to="/case-studies" replace />} />
-              <Route path="/how-we-work/" element={<Navigate to="/how-we-work" replace />} />
-              <Route path="/blog/" element={<Navigate to="/blog" replace />} />
-              <Route path="/resources/" element={<Navigate to="/resources" replace />} />
-              <Route path="/contact/" element={<Navigate to="/contact" replace />} />
-              <Route path="/sitemap/" element={<Navigate to="/sitemap" replace />} />
-              <Route path="/privacy-policy/" element={<Navigate to="/privacy-policy" replace />} />
-              <Route path="/terms-of-service/" element={<Navigate to="/terms-of-service" replace />} />
-              <Route path="/cookie-policy/" element={<Navigate to="/cookie-policy" replace />} />
-
-              {/* Old blog post URL redirect */}
-              <Route path="/blog/seo-no-murio-hype-estrategia" element={<Navigate to="/blog/seo-didnt-die-hype-strategy" replace />} />
-
-              {/* Main routes */}
               <Route path="/" element={<Index />} />
-              
               {/* Services SILO */}
               <Route path="/services" element={<Services />} />
               <Route path="/services/:service" element={<ServiceDetail />} />
               <Route path="/services/:service/:city" element={<ServiceCityDetail />} />
-              
               {/* Case Studies */}
               <Route path="/case-studies" element={<CaseStudies />} />
               <Route path="/case-studies/:slug" element={<CaseStudyDetail />} />
-              
               {/* Content */}
               <Route path="/how-we-work" element={<HowWeWork />} />
               <Route path="/blog" element={<Blog />} />
               <Route path="/blog/:slug" element={<BlogPost />} />
-              
               {/* Resources */}
               <Route path="/resources" element={<Resources />} />
               <Route path="/tools" element={<Resources />} />
-              
               {/* Company */}
               <Route path="/about" element={<About />} />
               <Route path="/contact" element={<Contact />} />
-              
               {/* Legal */}
               <Route path="/privacy-policy" element={<PrivacyPolicy />} />
               <Route path="/terms-of-service" element={<TermsOfService />} />
               <Route path="/cookie-policy" element={<CookiePolicy />} />
-              
               {/* Sitemap HTML */}
               <Route path="/sitemap" element={<Sitemap />} />
-              
               {/* Catch-all */}
               <Route path="*" element={<NotFound />} />
             </Routes>
